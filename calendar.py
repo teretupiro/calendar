9f55c4f50dbe17c6b25825c6b667b9f8c36bf333
teretupiro <vafin_2005@list.ru>
2022-11-21 19:58:21 +0300

    2.0

--- calendar.py
+++ calendar.py
@@ -1,10 +1,10 @@
+
 from tkinter import*
 from tkinter import ttk
 root=Tk()
-root.geometry('500x500')
+root.geometry('800x500')
 
-label = Label(root)
-label.pack()
+
 
 def create_event():
     # window=Tk()
@@ -23,14 +23,19 @@ def create_event():
     btn_ok.place(x=50, y=50)
 
 max=28
+week=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 for y in range(0,5):
     for x in range(1,8):
         if (x+y*7)<=max:
-         btn_1=ttk.Button(text=(x+y*7),width=10,command=create_event)
-         for y in range(0,5):
-             for x in range(1,8):
+         btn_1=ttk.Button(text=x+y*7,width=10,command=create_event).place(x=90*x,y=80*y,rely=0.1)
+         for i in week:
+            for o in range(0,7):
+             label_day=Label(justify=CENTER,text=week[o],width=9).place(x=90*x,rely=0.05)
+    for m in range(0,7):
+        for n in range (0,4):
+            if x+y*7:
+             label = Label(root,bg='red',width=7,height=3).place(x=90*m,y=80*n,relx=0.12,rely=0.15)
 
-              btn_1.place(x=0+(x+y*7),y=100+(x+y*7))
-
+ 
 
 root.mainloop()
